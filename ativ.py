9ce47ad22e72462674a1ba0ee4ce0a05e3ae5517
Caique Gerolim Santos <51054241821@info.local>
2024-05-03 17:27:47 -0300

    Codigos Python

--- ativ.py
+++
@@ -1,7 +0,0 @@
-contador = 0
-n =int(input("Quantas notas vai ter? "))
-while n in range(contador):
-    contador += n
-    n2 = int(input("Qual a nota do aluno? "))
-    media = (n2 * n)/2
-print(media)
